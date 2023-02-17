92c3023db215f4de66798831577f866b490d96de
Pavel Malai <Pavel.Malai@commify.com>
2023-02-17 14:14:17 +0200

    add pendo and fix build error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { combineLatest, filter, Subject, takeUntil } from 'rxjs';
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+declare let pendo: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,116 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pendo-lang-switch-poc';
+  pendoApiKey: string = '8303c8c6-d666-43eb-55ac-461d148b2fad';
+  private unsubscribe$: Subject<void> = new Subject();
+
+  constructor(
+    @Inject(DOCUMENT) private doc: Document
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.addPendoTag(this.pendoApiKey);
+  }
+
+  public ngAfterViewInit(): void {
+
+    // combineLatest([
+    //   this.userService.user$.pipe(filter((user: User) => !!user)),
+    //   this.accountService.selectedAccount$,
+    // ])
+    //   .pipe(takeUntil(this.unsubscribe$))
+    //   .subscribe(([user, account]: [User, UserAccount]) => {
+    //     if (this.pendoApiKey) {
+    //       const pendoConfig = {
+    //         visitor: {
+    //           id: user.id,
+    //           email: user.userDetails?.email,
+    //           full_name: `${user.userDetails?.firstName} ${user.userDetails?.surname}`,
+    //           role: account.userRole,
+    //         },
+    //         account: {
+    //           id: account.id,
+    //           reference: account.reference,
+    //           type: account.type,
+    //         },
+    //       };
+
+    //       if (!pendo?.isReady) {
+    //         setTimeout(() => this.installPendo(pendoConfig), 1000);
+
+    //         return;
+    //       }
+
+    //       this.installPendo(pendoConfig);
+    //     }
+    //   });
+
+
+    if (this.pendoApiKey) {
+      const pendoConfig = {
+        visitor: {
+          id: 'user.id',
+          email: 'user.userDetails?.email',
+          full_name: '`${user.userDetails?.firstName} ${user.userDetails?.surname}`',
+          role: 'account.userRole',
+        },
+        account: {
+          id: 'account.id',
+          reference: 'account.reference',
+          type: 'account.type',
+        },
+      };
+
+      if (!pendo?.isReady) {
+        setTimeout(() => this.installPendo(pendoConfig), 1000);
+
+        return;
+      }
+
+      this.installPendo(pendoConfig);
+    }
+  }
+
+  private installPendo(pendoConfig: unknown) {
+    if (pendo.isReady()) {
+      console.log('pendo is ready, identify Pendo')
+      pendo.identify(pendoConfig);
+    } else {
+      console.log('pendo is not ready, initialize Pendo')
+      pendo.initialize(pendoConfig);
+    }
+  }
+
+  private addPendoTag(apiKey: string): void {
+    console.log('addPendoTag');
+    if (!apiKey) {
+      return;
+    }
+
+    const pendoTag: HTMLScriptElement = this.doc.createElement('script');
+    pendoTag.type = 'text/javascript';
+    pendoTag.text = `
+        (function (apiKey) {
+            (function (p, e, n, d, o) {
+              var v, w, x, y, z;o = p[d] = p[d] || {};o._q = o._q || [];
+              v = ["initialize", "identify", "updateOptions", "pageLoad", "track"];
+              for (w = 0, x = v.length; w < x; ++w)
+                (function (m) {
+                  o[m] = o[m] || function () {
+                      o._q[m === v[0] ? "unshift" : "push"](
+                        [m].concat([].slice.call(arguments, 0))
+                      );
+                    };
+                })(v[w]);
+              y = e.createElement(n);
+              y.async = !0;
+              y.src = "https://cdn.eu.pendo.io/agent/static/" + apiKey + "/pendo.js";
+              z = e.getElementsByTagName(n)[0];
+              z.parentNode.insertBefore(y, z);
+            })(window, document, "script", "pendo")})
+            ('${apiKey}');`;
+
+    this.doc.head.appendChild(pendoTag);
+  }
 }
