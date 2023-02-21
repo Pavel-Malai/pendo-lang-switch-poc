9ad0cd2200534b0d7252344db17129649493b1fb
Pavel Malai <Pavel.Malai@commify.com>
2023-02-21 08:58:22 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -88,7 +88,7 @@ export class AppComponent {
           id: 'user.id',
           email: 'user.userDetails?.email',
           full_name: '`${user.userDetails?.firstName} ${user.userDetails?.surname}`',
-          language: 'de_DE'
+          language: 'de-DE'
         },
         account: {
           id: 'account.id',
